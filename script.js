0a890a96893fbddef89cdaadc23c2a21999bfeb7
Kenblocklive <kenblock@live.ru>
2023-11-30 22:32:22 +0400

    feat:check total bonuses

---
+++ script.js
@@ -0,0 +1,28 @@
+// const inputValue = document.querySelector("#value");
+// const divContainer = document.getElementsByTagName("h1")[0];
+// console.log(divContainer);
+
+// function convertEarnings(total) {
+//   const result = (total / 100) * 32;
+//   divContainer.innerText = `${result} баксов`;
+//   inputValue.value = null;
+//   return result;
+// }
+
+// const inputValue = document.querySelector("#value");
+// const divContainer = document.getElementsByTagName("h1")[0];
+// function handleFruitChange(total) {
+//   const selectElement = document.getElementById("hours");
+//   const selectedHours = +selectElement.value;
+//   if (selectedHours === 90) {
+//     const result = (total / 100) * 5;
+//     divContainer.innerText = `${result} баксов`;
+//     inputValue.value = null;
+//     return result;
+//   } else {
+//     const result = (total / 100) * 10;
+//     divContainer.innerText = `${result} баксов`;
+//     inputValue.value = null;
+//     return result;
+//   }
+// }
